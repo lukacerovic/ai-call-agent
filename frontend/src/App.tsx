e2f53857c66423a229d367056db17ca3b99da964
lukacerovic <124380132+lukacerovic@users.noreply.github.com>
2026-01-03 11:47:54 +0100

    Add main App component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CallInterface from './components/CallInterface';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CallInterface />
+    </div>
+  );
+}
+
+export default App;
